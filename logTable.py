afd058b7aa9277ac2098bd95dcf3f1739d333594
harimanca <87393376+harimanca@users.noreply.github.com>
2021-07-23 13:44:01 -0700

    Update logTable.py

--- logTable.py
+++ logTable.py
@@ -2,26 +2,31 @@ from pyo import *
 import matplotlib.pyplot as plt
 import math as math
 
+#function for Log Amplitude
 def amp(dist):
     return -0.231588*math.log(dist) + 0.970437
 
+#Boot and Start pyo server
 s = Server().boot().start()
 print('Server Booted')
 
 # sound path
 snd_path = 'sound path'
 
+# Takes User input for distance
 dist = int(input('Enter a distance: '))
 
+# Fills a list with amplitude multipliers based on input distance
 logTableList = []
 counter = 0
 for i in range(dist, 66):
     logTableList.append((counter, amp(i)))
     counter += 1
-print(logTableList)
 
+# Create LogTable with logTableList
 t = LogTable(logTableList)
 
+# Visual Plot of LogTable
 fig,ax = plt.subplots()
 plt.xlim(0, 66)
 ax.grid(color='black')
